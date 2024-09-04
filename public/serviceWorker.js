714e977e1649ca717efd6e144d35f54caf0c73f4
afnan102486 <afnanfarooq95@gmail.com>
2024-09-04 14:11:22 +0500

    commit 2

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,5 +1,9 @@
 const CACHE_NAME = "version-1";
 const urlsToCache = ["index.html", "offline.html"];
+const cacheResources = [
+  'index.js',
+  'app.css',
+]
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
